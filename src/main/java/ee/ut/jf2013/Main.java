0b27e7b12ca517091af0d28fe953266b68fded42
Jaroslav Judin <jaroslav.judin@elisa.fi>
2013-03-26 09:41:46 +0200

    Update main class.

--- src/main/java/ee/ut/jf2013/Main.java
+++ src/main/java/ee/ut/jf2013/Main.java
@@ -1,9 +1,9 @@
 package ee.ut.jf2013;
 
-import ee.ut.jf2013.homework5.MoneyTransfer;
+import ee.ut.jf2013.homework7.ImageTransformer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        MoneyTransfer.main(args);
+        ImageTransformer.main(args);
     }
 }
